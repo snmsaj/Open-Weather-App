adaf4d31182a27fc74d545065d894122a0bee71d
Shawn Saj <76454973+snmsaj@users.noreply.github.com>
2023-03-24 10:56:05 -0400

    fixed next.config file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "openweathermap.org" }],
   },
-  nextConfig,
 };
+
+module.exports = nextConfig;
