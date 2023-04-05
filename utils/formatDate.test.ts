a450d443772d38ae5f769f3fdadcb5ea88dcec24
Shawn Saj <76454973+snmsaj@users.noreply.github.com>
2023-04-05 12:30:46 -0400

    Added jest and formatDate test

---
+++ utils/formatDate.test.ts
@@ -0,0 +1,17 @@
+import formatDate from "./formatDate";
+
+describe("formatDate", () => {
+  it("should format a given unix timestamp to a 2-digit hour and minute time string", () => {
+    const unixTimestamp = 1649150400;
+    const expectedOutput = "05:20 AM";
+    expect(formatDate(unixTimestamp)).toEqual(expectedOutput);
+  });
+
+  it("should return null when time is undefined", () => {
+    expect(formatDate(undefined)).toBeNull();
+  });
+
+  it("should return null when time is not a number", () => {
+    expect(formatDate("not a number" as any)).toBeNull();
+  });
+});
